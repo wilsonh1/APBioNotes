7a5831a792d07f64dff111adf379b158a2b0f40c
Travis CI User <travis@example.org>
2020-08-18 17:24:18 +0000

    Update book

--- js/footnotes.js
+++ js/footnotes.js
@@ -4,10 +4,10 @@ for (let i = 0; i < fr.length; i++) {
     fr[i].onclick = function(e) {
         e.stopPropagation();
         let fb = fr[i].nextElementSibling;
-        if (fb.style.display == "none")
-            fb.style.display = "block";
-        else {
+        if (fb.style.display == "block")
             fb.style.display = "none";
+        else {
+            fb.style.display = "block";
         }
     }
 }
